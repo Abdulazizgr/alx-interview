ef97c7ae3deef72a8701a2e397ca9b56151cf147
Abdulaziz <abdulazizisa579@gmail.com>
2024-10-10 19:54:02 +0300

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,7 +6,7 @@ def canUnlockAll(boxes):
     '''determines if all the boxes can be opened or not
     Returns:
         True: all boxes can be opened
-        False: not all boxes can be opened
+        False: not boxes can be opened
     '''
     length = len(boxes)
     keys = set()
