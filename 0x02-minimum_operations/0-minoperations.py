e3a75665fdc9e2a781431907073ad7d4c730f466
abdulazizgr <abdulazizisa579@gmail.com>
2024-10-19 14:03:35 +0000

    test

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-
 """ Module for 0-minoperations"""
+
+
 def minOperations(n):
     """
     minOperations
     Gets fewest # of operations needed to result in exactly n H characters
     """
-    if n < 2:
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
         return 0
     ops, num = 0, 2
     while num <= n:
+        # if n evenly divides by num
         if n % num == 0:
+            # total even-divisions by root = total operations
             ops += num
-            n /= num
+            
+            n = n / num
+            
             num -= 1
+        
         num += 1
     return ops
-
